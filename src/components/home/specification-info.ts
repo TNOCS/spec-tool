c38429087aef9bd15b1bdebec60b0a3cf06bc680
Erik Vullings <erik.vullings@gmail.com>
2018-11-10 21:18:24 +0100

    Using the new Materialize components.

--- src/components/home/specification-info.ts
+++ src/components/home/specification-info.ts
@@ -1,5 +1,5 @@
 import m from 'mithril';
-import { inputText, inputTextArea } from './../../utils/html';
+import { TextInput, TextArea } from './../../utils/html';
 import { specSvc } from '../../services/spec-service';
 
 export const SpecificationInfo = () => ({
@@ -8,57 +8,48 @@ export const SpecificationInfo = () => ({
     const info = specSvc.specificationInfo;
     return m('.row', [
       m('h1', ti.docInfoTitle),
-      m(
-        inputTextArea({
-          id: 'author',
-          label: ti.authorLabel,
-          iconName: 'account_circle',
-          initialValue: info.author,
-          onchange: v => (info.author = v.toString()),
-        })
-      ),
-      m(
-        inputTextArea({
-          id: 'release',
-          label: ti.releaseLabel,
-          iconName: 'new_releases',
-          initialValue: info.releaseInfo,
-          onchange: v => (info.releaseInfo = v.toString()),
-        })
-      ),
-      m(
-        inputText({
-          id: 'version',
-          label: ti.versionLabel,
-          iconName: 'label',
-          initialValue: info.version,
-          onchange: v => (info.version = v.toString()),
-        })
-      ),
-      m(
-        inputText({
-          id: 'updated',
-          label: ti.updatedLabel,
-          iconName: 'event_available',
-          disabled: true,
-          initialValue: info.updated
-            ? info.updated.toLocaleDateString()
-            : new Date().toLocaleDateString(),
-          onchange: v => null,
-        })
-      ),
-      m(
-        inputText({
-          id: 'created',
-          label: ti.createdLabel,
-          iconName: 'event',
-          disabled: true,
-          initialValue: info.created
-            ? info.created.toLocaleDateString()
-            : new Date().toLocaleDateString(),
-          onchange: v => null,
-        })
-      ),
+      m(TextInput, {
+        id: 'author',
+        label: ti.authorLabel,
+        iconName: 'account_circle',
+        contentClass: 'col s12',
+        initialValue: info.author,
+        onchange: (v: string | number | boolean | Date) => (info.author = v.toString()),
+      }),
+      m(TextInput, {
+        id: 'version',
+        label: ti.versionLabel,
+        iconName: 'label',
+        contentClass: 'col s12',
+        initialValue: info.version,
+        onchange: (v: string | number | boolean | Date) => (info.version = v.toString()),
+      }),
+      m(TextArea, {
+        id: 'release',
+        label: ti.releaseLabel,
+        iconName: 'new_releases',
+        contentClass: 'col s12',
+        initialValue: info.releaseInfo,
+        onchange: (v: string | number | boolean | Date) => (info.releaseInfo = v.toString()),
+      }),
+      m(TextInput, {
+        id: 'updated',
+        label: ti.updatedLabel,
+        iconName: 'event_available',
+        contentClass: 'col s6',
+        disabled: true,
+        initialValue: info.updated ? info.updated.toLocaleDateString() : new Date().toLocaleDateString(),
+        onchange: () => null,
+      }),
+      m(TextInput, {
+        id: 'created',
+        label: ti.createdLabel,
+        iconName: 'event',
+        contentClass: 'col s6',
+        disabled: true,
+        initialValue: info.created ? info.created.toLocaleDateString() : new Date().toLocaleDateString(),
+        onchange: () => null,
+      }),
     ]);
   },
 });
